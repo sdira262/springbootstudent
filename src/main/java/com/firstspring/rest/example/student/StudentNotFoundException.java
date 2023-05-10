bb262e4f0add3670d6821187137d463dbdb8ec30
subramanian <subramanian@kmccorp.in>
2023-05-10 12:56:37 +0530

    Changed sample

--- src/main/java/com/firstspring/rest/example/student/StudentNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.firstspring.rest.example.student;
-
-public class StudentNotFoundException extends RuntimeException {
-
-    public StudentNotFoundException(String exception) {
-        super(exception);
-    }
-
-}
--- src/main/java/com/firstspring/rest/example/student/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.firstspring.rest.example.student;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
-
-}
